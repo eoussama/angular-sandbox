993a28da7ea9b5cf2802dac0b92c06b1ffe7106f
EOussama <oussama_gtacj@live.fr>
2023-03-01 12:15:27 +0100

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { RouterModule } from '@angular/router';
+import { GuestGuard } from './guards/guest/guest.guard';
+import { AuthenticationGuard } from './guards/authentication/authentication.guard';
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot([
+      {
+        path: 'home',
+        canActivateChild: [AuthenticationGuard],
+        loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+      },
+      {
+        path: 'login',
+        canActivateChild: [GuestGuard],
+        loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)
+      },
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
+      },
+      {
+        path: '**',
+        redirectTo: ''
+      }
+    ])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
+import { userReducer } from './state/auth.reducer';
+import { AppRoutingModule } from './app-routing.module';
+import { EffectsModule } from '@ngrx/effects';
+import { AuthEffects } from './state/auth.effect';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    EffectsModule.forRoot([AuthEffects]),
+    StoreModule.forRoot({ user: userReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
