0225123a68263940472730b20725100c664fff1c
EOussama <oussama_gtacj@live.fr>
2023-03-03 13:23:05 +0100

    Detail page info

--- src/app/models/superhero.model.ts
+++ src/app/models/superhero.model.ts
@@ -6,6 +6,36 @@ export class Superhero {
   name: string;
   image: string;
 
+  stats: {
+    combat: number;
+    durability: number;
+    intelligence: number;
+    power: number;
+    speed: number;
+    strength: number;
+  };
+
+  bases: Array<string>;
+  occupation: string;
+
+  race: string;
+  gender: string;
+  height: number;
+  weight: number;
+  eyeColor: string;
+  hairColor: string;
+
+  debute: string;
+  alterEgo: string;
+  fullName: string;
+  publisher: string;
+  alignment: string;
+  birthPlace: string;
+  aliases: Array<string>;
+
+  affiliations: Array<string>;
+  relatives: Array<string>;
+
   /**
    * @description
    * If the superhero is favorited.
@@ -13,9 +43,40 @@ export class Superhero {
   favorite: boolean;
 
   constructor(model: ISuperheroBaseResponse, isFavorite: boolean = false) {
-    this.id = parseInt(model.id);
-    this.name = model.name;
-    this.image = model.image.url;
+    this.name = model.name ?? '';
+    this.id = parseInt(model.id ?? '');
+    this.image = model.image.url ?? '';
+
+    this.occupation = model.work.occupation ?? '';
+    this.bases = model.work.base.split(', ') ?? [];
+
+    this.race = model.appearance.race ?? '';
+    this.gender = model.appearance.gender ?? '';
+    this.eyeColor = model.appearance['eye-color'] ?? '';
+    this.hairColor = model.appearance['hair-color'] ?? '';
+    this.height = parseInt(model.appearance.height[1] ?? '');
+    this.weight = parseInt(model.appearance.weight[1] ?? '');
+
+    this.aliases = model.biography.aliases ?? [];
+    this.publisher = model.biography.publisher ?? '';
+    this.alignment = model.biography.alignment ?? '';
+    this.fullName = model.biography['full-name'] ?? '';
+    this.alterEgo = model.biography['alter-egos'] ?? '';
+    this.debute = model.biography['first-appearance'] ?? '';
+    this.birthPlace = model.biography['place-of-birth'] ?? '';
+
+    this.relatives = model.connections.relatives.split(', ');
+    this.affiliations = model.connections['group-affiliation'].split(', ');
+
+    this.stats = {
+      power: parseInt(model.powerstats.power ?? ''),
+      speed: parseInt(model.powerstats.speed ?? ''),
+      combat: parseInt(model.powerstats.combat ?? ''),
+      strength: parseInt(model.powerstats.strength ?? ''),
+      durability: parseInt(model.powerstats.durability ?? ''),
+      intelligence: parseInt(model.powerstats.intelligence ?? '')
+    }
+
     this.favorite = isFavorite;
   }
 }
