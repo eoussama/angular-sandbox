4e4aec5744041e86bb83f9d311d81a270af7ab9c
EOussama <oussama_gtacj@live.fr>
2023-03-21 16:12:54 +0000

    Input stories

---
+++ src/stories/Input.stories.ts
@@ -0,0 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { ControlContainer, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { moduleMetadata } from '@storybook/angular';
+import { Story, Meta } from '@storybook/angular/types-6-0';
+import { InputComponent } from 'src/app/components/input/input.component';
+
+export default {
+  title: 'Components/Input',
+  excludeStories: /.*Data$/,
+  component: InputComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [],
+      imports: [CommonModule, FormsModule, ReactiveFormsModule],
+      providers: [ControlContainer]
+    })
+  ]
+} as Meta;
+
+const Template: Story = args => ({
+  props: {
+    ...args
+  },
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  label: 'Input'
+};
+
+export const Password = Template.bind({});
+Password.args = {
+  label: 'Password',
+  type: 'password'
+};
+
+export const Icon = Template.bind({});
+Icon.args = {
+  label: 'Login',
+  icon: 'person'
+};
